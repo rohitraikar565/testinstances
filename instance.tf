47e4d61a0b3fc293514a5915d7b3237a194f3e88
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-04-24 16:37:15 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,20 @@
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+ tags = ["lxi", "sxi"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+  network_interface {
+    network = "default"
+  }
+}
