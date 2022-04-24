60dd010120ceb0a0e2a5960f8a13cd8284e7b25a
rohitraikar565 <61474353+rohitraikar565@users.noreply.github.com>
2022-04-24 18:25:43 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 provider "google" {
   credentials = "${file("inframod-training-09efb368bf98.json")}"
   project     = "inframod-training"
-  zone        = "asia-south1-a"
+  availability_zone       = [us-central1-a, us-central1-f, us-central1-b, us-central1-c, asia-south1-c, asia-south1-b]
 }
 resource "google_compute_instance" "default1" {
   name         = var.name
